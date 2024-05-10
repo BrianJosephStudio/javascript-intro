1bdcfd3e5f28c61a0432d7d4784e2e6a4545069b
Brian Joseph Studio <brianjosephstudio@gmail.com>
2024-05-10 19:02:41 +0200

    Refactored App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import './App.css'
-import { useRef } from 'react';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import { PageOne } from './components/PageOne.tsx'
+import { CustomerForm } from './components/CustomerForm.tsx';
 
 function App() {
-  const containerRef = useRef<HTMLDivElement>(null);
   const router = createBrowserRouter([
     {
       path: "/1",
-      element: <PageOne/>,
+      element: <PageOne />,
+    },
+    {
+      path: "/customer-form",
+      element: <CustomerForm />,
     },
   ])
 
   return (
-    <>
-      <div ref={containerRef} className="container" onClick={_event => console.log("clicked")}>
-        <RouterProvider router={router} />
-      </div>
-    </>
+      <RouterProvider router={router} />
   )
 }
 
