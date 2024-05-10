43430f4cc7e31321b8f1263302fb8fa584e9d752
Brian Joseph Studio <brianjosephstudio@gmail.com>
2024-05-10 19:02:55 +0200

    refactored CustomerForm

--- src/components/CustomerForm.tsx
+++ src/components/CustomerForm.tsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect, useRef } from 'react';
+import { ThemeSwitch } from './ThemeSwitch';
 
 interface FormData {
   name: string;
@@ -8,49 +8,87 @@ interface FormData {
 }
 
 export const CustomerForm: React.FC = () => {
-  const appUrl = `/${import.meta.env.VITE_APP_URL}`
-  const lightModeUrl = `${appUrl}/light-mode`
-  // const darkModeUrl = `${appUrl}/dark-mode`
+  const logo = useRef<HTMLImageElement>(null)
+  const container = useRef<HTMLDivElement>(null)
+  const header = useRef<HTMLHeadingElement>(null)
 
+
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("light")
   const [formData, setFormData] = useState<FormData>({
-    name: 'John',
-    email: 'Doe',
+    name: 'John Doe',
+    email: 'johndoe@email.com',
     phone: '+13024562353'
   });
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-      const { name, value } = e.target;
-      setFormData({
-        ...formData,
-        [name]: value
-      });
-    };
-    
-    const handleSubmitNoJs = (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      
-      const url = `/dark-mode`
-      axios.get(url)
-    };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleThemeSwap = () => {
+    console.log("check", themeMode)
+    setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    if (
+      !logo.current ||
+      !container.current ||
+      !header.current
+    ) { return }
+
+    logo.current.src = logo.current.src.replace(/(light)|(dark)/, themeMode)
+    container.current.style.backgroundColor = themeMode === 'light' ? '#f9f9f9' : '#2e2e2e'
+    header.current.style.color = themeMode === 'light' ? '#3d3d3d' : 'white'
+
+    const labels = [...container.current!.querySelectorAll('label')]
+    const inputs = [...container.current!.querySelectorAll('input')]
+
+    labels.forEach((label) => label.style.color = themeMode === 'light' ? '#3d3d3d' : 'white')
+    inputs.forEach((input) => {
+      if (themeMode === "light") {
+        input.style.color = '#3d3d3d'
+        input.style.backgroundColor = 'white'
+        input.style.borderColor = '#cccccc'
+      } else if (themeMode === 'dark') {
+        input.style.color = 'white'
+        // input.style.backgroundColor = '#3d3d3d'
+        input.style.backgroundColor = '#2e2e2e'
+        input.style.borderColor = '#616161'
+      }
+    })
+  }, [themeMode])
 
   return (
-      <div>
-        <h2>Customer Information</h2>
-        <form onSubmit={handleSubmitNoJs}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="phone">Phone Number:</label>
-            <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required />
-          </div>
-          <button type="submit" className="btn">Submit</button>
-        </form>
+    <div ref={container} className='container'>
+      <div className="navBar">
+        <div className="logo">
+          <img ref={logo} src="./compass-logo-light.svg" alt=""></img>
+        </div>
+        <div onClick={handleThemeSwap}>
+          <ThemeSwitch theme={themeMode}></ThemeSwitch>
+        </div>
       </div>
+      <h2 ref={header}>Contact Details</h2>
+      <form onSubmit={undefined}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone Number:</label>
+          <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required />
+        </div>
+        <button type="submit" className="btn">Next</button>
+      </form>
+    </div>
   );
 };
 
