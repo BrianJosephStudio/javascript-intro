02806db50dd5975f5be40e3371a58b5202cf2969
Brian Joseph Studio <brianjosephstudio@gmail.com>
2024-05-11 19:32:04 +0200

    Integrate theme mode

--- src/components/InvalidInputAlert.tsx
+++ src/components/InvalidInputAlert.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react"
+import { UserDetailsContext } from "../util/context/UserDetailsContext"
+
 const defaultErrorMessage = "invalid"
 
 export const InvalidInputAlert = ({
@@ -7,10 +10,16 @@ export const InvalidInputAlert = ({
     visible?: boolean,
     message?: string
 }) => {
+    //@ts-ignore
+    const { theme } = useContext(UserDetailsContext)
+    const styles = {
+        backgroundColor: theme === 'light' ? 'hsl(0, 50%, 72%)' : 'rgb(193 79 79)',
+        opacity: !!visible ? 1 : 0
+        
+    }
+
     return (
-        <div id="invalidInput" data-invalid={visible} className="invalidAlert" style={{
-            opacity: !!visible ? 1 : 0
-        }}>
+        <div id="invalidInput" data-invalid={visible} className="invalidAlert" style={styles}>
             {message}
         </div>
     )
