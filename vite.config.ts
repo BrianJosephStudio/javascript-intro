269f707964f62d66c2bc83c645f40cbf7abb1367
Brian Joseph Studio <brianjosephstudio@gmail.com>
2024-05-19 23:54:13 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apps/javascript-intro/',
   plugins: [react()],
   build: {
     target: 'esnext',
-    outDir: "build"
+    outDir: "../../backend/apps/javascript-intro"
   },
   server: {
     port: 5002,
